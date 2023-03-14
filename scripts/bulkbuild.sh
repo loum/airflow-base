50053f93090f25843699aa61c632307bf080c21b
Lou Markovski <lomarkovski@seek.com.au>
2023-03-14 14:52:23 +1100

    3-support-for-docker-multi-platform-builds

---
+++ scripts/bulkbuild.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+for AIRFLOW_VERSION in 2.3.3 2.3.4 2.4.0 2.4.1 2.4.2
+do
+    MAKESTER__DOCKER_PLATFORM=linux/arm64,linux/amd64\
+ MAKESTER__DOCKER_DRIVER_OUTPUT=push\
+ AIRFLOW_VERSION=$AIRFLOW_VERSION\
+ make image-buildx
+done
