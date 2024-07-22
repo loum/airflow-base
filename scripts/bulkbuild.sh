9418e8f156405601f2b641dfe85796cf3a24c3dd
Lou Markovski <lou@calcium.local>
2024-07-22 12:36:57 +1000

    45-lifecycle-management-airflow-292

--- scripts/bulkbuild.sh
+++ scripts/bulkbuild.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for AIRFLOW_VERSION in 2.7.2 2.7.3 2.8.0 2.8.1 2.8.2 2.8.3 2.8.4 2.9.0 2.9.1
+for AIRFLOW_VERSION in 2.7.2 2.7.3 2.8.0 2.8.1 2.8.2 2.8.3 2.8.4 2.9.0 2.9.1 2.9.2
 do
     MAKESTER__DOCKER_PLATFORM=linux/arm64,linux/amd64\
  MAKESTER__DOCKER_DRIVER_OUTPUT=push\
